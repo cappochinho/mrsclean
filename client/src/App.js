c9838b0fa91031e08eee3b3fb542195592a618ab
cappochinho <edwardprempeh18@gmail.com>
2023-06-08 01:47:04 +0100

    Testing React components

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState } from 'react';
+import Gallery from './AppComponents/NavigationBar';
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, []);
+
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+      <Gallery />
+    </div>
+  )
+}
+
+export default App

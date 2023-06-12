aa2c54b89fcfc0c7d07c3acc0195406a33bf7072
cappochinho <edwardprempeh18@gmail.com>
2023-06-12 20:29:30 +0100

    Added the Products page + Products css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react';
+import React from 'react';
 import { Logo, UpperText } from "../AppComponents/Header";
 import { Images, Videos } from "../AppComponents/Body";
 import { AddressSection} from "../AppComponents/Footer";
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import { Logo } from '../AppComponents/Header';
+import './styles/Products.css'
 
 export default function Products() {
   return (
-    <h1>Mrs Clean Products</h1>
+    <div>
+      <Logo />
+      <h1 style={{ textAlign: 'center' }}>Products</h1>
+      <div style={{ padding: '30px' }}>
+        <div style={
+        { position: 'relative', display: 'flex', flexDirection: 'column',
+          justifyContent: 'center', alignItems: 'center',  border: '2px solid whitesmoke',
+          objectFit: 'cover', padding: '0', margin: '0',
+        }
+      }>
+      <div className='gimagecontainer'>
+        <img
+          className='greenimage'
+          src={require("../AppComponents/images/green.jpg")}
+          alt="Green Paste"
+          width={200}
+          height={200}
+          style={
+            {
+              borderRadius: '10px',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              padding: '10px',
+              display: 'block',
+            }
+          }
+        />
+        <div className='greentext'>Green Paste</div>
+      </div>
+      <div className='yimagecontainer'>
+        <img
+          className='yellowimage'
+          src={require("../AppComponents/images/yellow.jpg")}
+          alt="Yellow Paste"
+          width={200}
+          height={200}
+          style={
+            {
+              borderRadius: '10px',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              padding: '10px'
+            }
+          }
+        />
+        <div className='yellowtext'>Yellow Paste</div>
+      </div>
+    </div>
+      </div>
+    </div>
+
   );
 }
